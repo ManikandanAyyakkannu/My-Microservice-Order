3b48cf9f1af304296eccb1283c2eab5d25ce1f99
MANIKANDAN AYYAKKANNU <maniia88@gmail.com>
2025-09-22 15:43:45 +0530

    chnages

--- src/main/java/my/microservice/order_service/Controller/ProductClient.java
+++ src/main/java/my/microservice/order_service/Controller/ProductClient.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "product-service", url = "http://my-microservice-products-git-maniia88-dev.apps.rm1.0a51.p1.openshiftapps.com")
+@FeignClient(name = "product-service", url = "https://my-microservice-products-git-maniia88-dev.apps.rm1.0a51.p1.openshiftapps.com")
 public interface ProductClient {
 
     @GetMapping("/products")
